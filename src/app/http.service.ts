ae428e794149d14bb6d9523186df3c44bed3a2f7
Alex <43249576+Ajv97@users.noreply.github.com>
2022-02-09 11:46:36 -0600

    uses local api

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,7 +9,7 @@ export class HttpService {
   constructor(private _http: HttpClient) { }
 
   urlBase = 'http://api.alexvanbeekum.com:8080/api';
-  // urlBase='http://localhost:8080/api';
+  urlBase='http://localhost:8080/api';
 
 
   getBoxesId(id){
